fff9372a17da659674577c807a7f930fbacb2b4a
nabiluddin <20egics072@gits.ac.in>
2024-03-02 23:21:06 +0530

    remove './reportWebVitals' import

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { ClerkProvider, SignUp, SignedIn, SignedOut } from "@clerk/clerk-react";
 import { useNavigate,Routes,Route,BrowserRouter } from 'react-router-dom';
 import About from './Components/About';
@@ -62,8 +61,3 @@ root.render(
     </BrowserRouter>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
